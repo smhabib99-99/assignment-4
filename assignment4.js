53c4c027779e103c88a36c4b9b782bbe14ee8e41
smhabib99-99 <habibaust34@gmail.com>
2023-08-09 08:42:06 +0600

    problem 03 done

--- assignment4.js
+++ assignment4.js
@@ -23,3 +23,30 @@ function matchFinder(string1, string2) {
         }
     }
 }
+
+
+
+
+
+function sortMaker(arr) {
+
+    const temp = [2];
+
+    if (arr[0] > 0 && arr[1] > 0) {
+        if (arr[0] > arr[1]) {
+            return arr;
+        }
+        else if(arr[0] < arr[1]){
+            const temp = arr[0];
+            arr[0] = arr[1];
+            arr[1] = temp;
+            return arr;
+        }else{
+            return 'equal';
+        }
+    }
+    else{
+        return 'Invalid Input';
+    }
+
+}
--- test4.js
+++ test4.js
@@ -25,19 +25,51 @@
 // problem #02
 // ============================================
 
-function matchFinder(string1, string2) {
+// function matchFinder(string1, string2) {
 
-    if(typeof string1 !== 'string' || typeof string2 !== 'string'){
-        return 'please enter string type input.'
-    }
-    else{
-        if(string1.indexOf(string2) !== -1){
-            return true;
+//     if(typeof string1 !== 'string' || typeof string2 !== 'string'){
+//         return 'please enter string type input.'
+//     }
+//     else{
+//         if(string1.indexOf(string2) !== -1){
+//             return true;
+//         }else{
+//             return false;
+//         }
+//     }
+// }
+
+// const checkMatch =matchFinder("Peter Parker","pet");
+// console.log(checkMatch);
+
+
+// problem #03
+// ============================================
+
+function sortMaker(arr) {
+
+    const temp = [2];
+
+    if (arr[0] > 0 && arr[1] > 0) {
+        if (arr[0] > arr[1]) {
+            return arr;
+        }
+        else if(arr[0] < arr[1]){
+            const temp = arr[0];
+            arr[0] = arr[1];
+            arr[1] = temp;
+            return arr;
         }else{
-            return false;
+            return 'equal';
         }
     }
+    else{
+        return 'Invalid Input';
+    }
+
 }
 
-const checkMatch =matchFinder("Peter Parker","pet");
-console.log(checkMatch);
+
+let x = [2, 3];
+const checkOutputArray = sortMaker(x);
+console.log(checkOutputArray);
